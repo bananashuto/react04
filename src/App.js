152b0f9db23232446cd7b7fc7c67c9c175300e50
村上　紀行 <69379359+bananashuto@users.noreply.github.com>
2020-12-02 23:07:25 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Feed from "./Feed";
+const App = () => {
+  return (
+    <div>
+      <h1>インスタグラム風アプリ</h1>
+      <Feed />
+    </div>
+  );
+};
+export default App;
